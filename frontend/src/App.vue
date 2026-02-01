152a811d4e96e2d92b4b2cbfad98ec9b76f0ae55
chmunn <chmunn@crimson.ua.edu>
2026-01-31 18:35:22 -0600

    Feat: Add frontend structure, container
    Fix: Change 'web' to 'backend' in docker-compose for readability

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Hello World for Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
