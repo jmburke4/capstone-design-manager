dd56b53bd9fc7b5693fd47c6bdb7cc723beff34b
Gates <retchwertz@gmail.com>
2026-02-12 13:59:02 -0600

    Feat: add assignment table to backend

--- backend/project/serializers.py
+++ backend/project/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Assignment
 
 class ProjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Project
         fields = ['id', 'name', 'description', 'sponsor', 'website', 'created_at', 'status']
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = "__all__"
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet
+from .views import ProjectViewSet, AssignmentViewSet
 
 app_name = 'project'
 
@@ -9,6 +9,7 @@ router = DefaultRouter()
 # The first parameter is the route used to access the resource
 # The third field is the name of the Python class that you added
 router.register(r'projects', ProjectViewSet, basename='project')
+router.register(r'assignments', AssignmentViewSet, basename='assignment')
 
 urlpatterns = [
     path('', include(router.urls)),
