5833115e625c6d08da6892010508555c8ec99e26
ban <jmburke4@crimson.ua.edu>
2026-03-04 14:18:49 -0600

    Chore: Configure Django logging

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -138,3 +138,36 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            'level': 'DEBUG',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
--- backend/project/views.py
+++ backend/project/views.py
@@ -1,11 +1,19 @@
 from rest_framework import viewsets
 from .models import Project, Assignment, Preference
-from .serializers import ProjectSerializer, AssignmentSerializer, PreferenceSerializer
+from .serializers import ProjectSerializer, AssignmentSerializer, PreferenceSerializer, PreferenceBulkSerializer
+from django.db.models import Q
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    def list(self, request, *args, **kwargs):
+        logger.debug("Listing projects")
+        return super().list(request, *args, **kwargs)
 
 
 class PreferenceViewSet(viewsets.ModelViewSet):
