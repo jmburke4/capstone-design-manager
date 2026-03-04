61a0de629f3e82edee10830e56a1576d4b99133e
ban <jmburke4@crimson.ua.edu>
2026-03-04 17:31:12 +0000

    Feat: Update student model to include CWID and other information, update admin panel to display more fields when listing records

--- backend/project/views.py
+++ backend/project/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import Project, Assignment, Preference
 from .serializers import ProjectSerializer, AssignmentSerializer, PreferenceSerializer
 
+
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
@@ -10,6 +11,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
 class PreferenceViewSet(viewsets.ModelViewSet):
     queryset = Preference.objects.all()
     serializer_class = PreferenceSerializer
+
 
 class AssignmentViewSet(viewsets.ModelViewSet):
     queryset = Assignment.objects.all()
