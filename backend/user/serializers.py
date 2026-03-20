c12c8e98b681add04d2b7c04734ab87141412522
ban <jmburke4@crimson.ua.edu>
2026-03-20 15:31:03 -0400

    Fix: Update compose script to copy frontend environment file and reorganize example environment files

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -4,16 +4,28 @@ from .models import Student
 
 
 class SponsorSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(read_only=True)
 
     class Meta:
         model = Sponsor
         fields = '__all__'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Create only fields
+        if self.instance:
+            self.fields['email'].read_only = True
+
 
 class StudentSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(read_only=True)
 
     class Meta:
         model = Student
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Create only fields
+        if self.instance:
+            self.fields['email'].read_only = True
